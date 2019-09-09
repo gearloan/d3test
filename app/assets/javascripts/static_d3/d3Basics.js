51317e326240c37de12a812169e9ae68a56f6577
gearloan <johnsfuller@gmail.com>
2019-09-09 13:59:27 -0400

    finished second tutorial

--- app/assets/javascripts/static_d3/d3Basics.js
+++ app/assets/javascripts/static_d3/d3Basics.js
@@ -1,6 +1,8 @@
 window.onload = function () {
 
   ////////////////////////// Tutorial 1 //////////////////////////
+  // Display some stuff, static data
+
   var vis = d3.select("#graph")
               .append("svg")
   .attr("width", 200).attr("height", 80);
@@ -36,15 +38,43 @@ window.onload = function () {
      .style("stroke", "rgb(6,120,155)");
 
   ////////////////////////// Tutorial 2 //////////////////////////
+  // make a chart, load data
 
-  var dataX = [4, 8, 15, 16, 23, 42];
+  var width = 420,
+      barHeight = 20;
 
-  d3.select(".basic-bar-chart")
-    .selectAll("div")
-      .data(dataX)
-    .enter().append("div")
-      .style("width", function(d) { return d * 10 + "px"; })
-      .text(function(d) { return d; });
+  var x = d3.scale.linear()
+    .range([0, width]);
 
+  var chart = d3.select(".chart")
+      .attr("width", width);
 
+  d3.tsv("https://coab-alchemy-attachments.s3.amazonaws.com/data.tsv", type, function(error, data) {
+    x.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+    chart.attr("height", barHeight * data.length);
+
+    var bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+        .attr("width", function(d) { return x(d.value); })
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function(d) { return x(d.value) - 3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.value; });
+  });
+
+  function type(d) {
+    d.value = +d.value; // coerce to number
+    return d;
+  }
+
+  ////////////////////////// Tutorial 3 //////////////////////////
+  // exploring various chart configurations
 }
