d3ab94910cb1ebf3f92a1ae0b2368bfa6b20b687
gearloan <johnsfuller@gmail.com>
2019-09-09 11:52:53 -0400

    setup static pages

--- app/controllers/static_d3_controller.rb
+++ app/controllers/static_d3_controller.rb
@@ -1,4 +1,10 @@
 class StaticD3Controller < ApplicationController
   def index
   end
+  def pieChart
+  end
+  def barChart
+  end
+  def basicTutorials 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   root 'static_d3#index'
 
-  get 'static_d3/index'
+  get '/pie-chart' => 'static_d3#pieChart', as: 'pieChart'
+  get '/bar-chart' => 'static_d3#barChart', as: 'barChart'
+  get '/basics' => 'static_d3#basicTutorials', as: 'basicTutorials'
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
