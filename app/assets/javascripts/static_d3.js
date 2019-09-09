f9124e0e0beda01b3a68095288ff7d6493461325
gearloan <johnsfuller@gmail.com>
2019-09-09 11:34:11 -0400

    testing: d3 setup

--- app/assets/javascripts/static_d3.js
+++ app/assets/javascripts/static_d3.js
@@ -1,22 +1,21 @@
+window.onload = function () {
+  var vis = d3.select("#graph")
+              .append("svg")
+  .attr("width", 200).attr("height", 200);
 
+  var nodes = [
+      {x: 10, y: 50},
+      {x: 70, y: 10},
+      {x: 140, y: 50}
+    ]
 
+  vis.selectAll("circle.nodes")
+     .data(nodes)
+     .enter()
+     .append("svg:circle")
+     .attr("cx", function(d) { return d.x; })
+     .attr("cy", function(d) { return d.y; })
+     .attr("r", "10px")
+     .attr("fill", "black")
 
-var vis = d3.select("#graph")
-            .append("svg")
-.attr("width", 200).attr("height", 200);
-
-var nodes = [
-    {x: 10, y: 50},
-    {x: 70, y: 10},
-    {x: 140, y: 50}
-  ]
-
-vis.selectAll("circle.nodes")
-   .data(nodes)
-   .enter()
-   .append("svg:circle")
-   .attr("cx", function(d) { return d.x; })
-   .attr("cy", function(d) { return d.y; })
-   .attr("r", "10px")
-   .attr("fill", "black")
-   
+}
